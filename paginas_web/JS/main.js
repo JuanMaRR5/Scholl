9929d56f276ebdbfba877001f06bf7612437d5d1
JuanMaRR5 <juanmarr5@gmail.com>
2024-07-30 20:36:33 -0300

    paginas casi finalizadas solo faltan la foto de los productos

--- paginas_web/JS/main.js
+++ paginas_web/JS/main.js
@@ -11,11 +11,5 @@ cerrar.addEventListener("click", () =>{
 })
 
 
-let hideText_btn = document.getElementById('hideText_btn')
-let hideText = document.getElementById('hideText')
 
-hideText_btn.addEventListener('click', toggleText);
 
-function toggleText(){
- hideText.classList.toggle('show');
-}
