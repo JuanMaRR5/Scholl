6187b11fdec459957d3e83ffd28d36d95e4cfa60
JuanMaRR5 <juanmarr5@gmail.com>
2024-10-08 16:23:51 -0300

    termine la pagina

--- paginas_web/JS/main.js
+++ paginas_web/JS/main.js
@@ -10,6 +10,149 @@ cerrar.addEventListener("click", () =>{
     nav.classList.remove("visible");
 })
 
+function mostrar(){
+    document.getElementById('img').style.display = 'block'
+}
+
+function ocultar(){
+    document.getElementById('img').style.display = 'none'
+}
+
+
+function mostrar2(){
+    document.getElementById('img2').style.display = 'block'
+}
+
+function ocultar2(){
+    document.getElementById('img2').style.display = 'none'
+}
+
+
+function mostrar3(){
+    document.getElementById('img3').style.display = 'block'
+}
+
+function ocultar3(){
+    document.getElementById('img3').style.display = 'none'
+}
+
+function mostrar4(){
+    document.getElementById('img4').style.display = 'block'
+}
+
+function ocultar4(){
+    document.getElementById('img4').style.display = 'none'
+}
+
+
+function mostrar5(){
+    document.getElementById('img5').style.display = 'block'
+}
+
+function ocultar5(){
+    document.getElementById('img5').style.display = 'none'
+}
+
+
+function mostrar6(){
+    document.getElementById('img6').style.display = 'block'
+}
+
+function ocultar6(){
+    document.getElementById('img6').style.display = 'none'
+}
+
+
+function mostrar7(){
+    document.getElementById('img7').style.display = 'block'
+}
+
+function ocultar7(){
+    document.getElementById('img7').style.display = 'none'
+}
+
+function mostrar8(){
+    document.getElementById('img8').style.display = 'block'
+}
+
+function ocultar8(){
+    document.getElementById('img8').style.display = 'none'
+}
+
+function mostrar9(){
+    document.getElementById('img9').style.display = 'block'
+}
+
+function ocultar9(){
+    document.getElementById('img9').style.display = 'none'
+}
+
+function mostrar10(){
+    document.getElementById('img10').style.display = 'block'
+}
+
+function ocultar10(){
+    document.getElementById('img10').style.display = 'none'
+}
+
+function mostrar11(){
+    document.getElementById('img11').style.display = 'block'
+}
+
+function ocultar11(){
+    document.getElementById('img11').style.display = 'none'
+}
+
+function mostrar12(){
+    document.getElementById('img12').style.display = 'block'
+}
+
+function ocultar12(){
+    document.getElementById('img12').style.display = 'none'
+}
+
+function mostrar13(){
+    document.getElementById('img13').style.display = 'block'
+}
+
+function ocultar13(){
+    document.getElementById('img13').style.display = 'none'
+}
+
+function mostrar14(){
+    document.getElementById('img14').style.display = 'block'
+}
+
+function ocultar14(){
+    document.getElementById('img14').style.display = 'none'
+}
+
+function mostrar15(){
+    document.getElementById('img15').style.display = 'block'
+}
+
+function ocultar15(){
+    document.getElementById('img15').style.display = 'none'
+}
+
+function mostrar16(){
+    document.getElementById('img16').style.display = 'block'
+}
+
+function ocultar16(){
+    document.getElementById('img16').style.display = 'none'
+}
+
+function mostrar17(){
+    document.getElementById('img17').style.display = 'block'
+}
+
+function ocultar17(){
+    document.getElementById('img17').style.display = 'none'
+}
+
+
+
 
 
 
